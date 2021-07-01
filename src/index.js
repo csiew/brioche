e10b5d211e46bd4b4e758ac2074fc44d85c516c2
Clarence Siew <clarence.siew@gmail.com>
2021-07-01 17:59:36 +1000

    Export styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+/*
+ *  STYLING
+ */
+
+// CSS utility
+export * from '../styles/Util.css';
+
+// Aesthetic definitions 
+export * from '../styles/Definitions.css';
+export * from '../styles/themes/Default.css';
+export * from '../styles/themes/Dark.css';
+
+// Controls and sheets
+export * from '../styles/Main.css';
+export * from '../styles/Controls.css';
+export * from '../styles/Surfaces.css';
+export * from '../styles/Lists.css';
+export * from '../styles/Article.css';
+
+// Animation
+export * from '../styles/Animation.css';
+export * from '../styles/Interactive.css';
+
+// Fonts
+export * from '../assets/fonts/PublicSans.css';
+export * from '../assets/fonts/Bitter.css';
+export * from '../assets/fonts/RobotoMono.css';
+
+/*
+ *  COMPONENTS
+ */
+
 // Controls
 export * from './components/controls/Button.jsx';
 export * from './components/controls/SelectableList.jsx';
